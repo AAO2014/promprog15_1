d3b82a10ed5cea5596d8adeb9daae22c45f2fe9c
Avis <aao.t@yandex.ru>
2015-10-11 22:15:12 +0300

    lesson4 test

--- lesson4/classwork_3.py
+++ lesson4/classwork_3.py
@@ -9,14 +9,31 @@
 # например 1700, 1800 и 1900 — невисокосные года, 2000 — високосный.
 
 from classwork_2 import is_leap_year
+import math
+
+class NonValidDataExeption
 
 def yesterday(year, month, day):
+
+    try
+        if (0 <= int(year) <= 9999) and (0 < int(month) < 13) and (0 < int(day) < 32):
+            if
+
     lenOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     if is_leap_year(year):
         lenOfMonths[1] = 29
-    y = 1
-    m =1
-    d =1
+    if (month == 1) and (day == 1):
+        y = year - 1
+        m = 12
+        d = 31
+    else:
+        y = year
+        if day == 1:
+            m = month - 1
+            d = lenOfMonths[m - 1]
+        else:
+            m = month
+            d = day - 1
+
     return y, m, d
 
-yesterday(2000,1,1)
