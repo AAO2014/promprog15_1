e85b02baa1c98de3ba857e7b61bcca6a3fe61f6f
wad <suguby@gmail.com>
2015-10-15 22:16:30 +0300

    проверка задания classwork_3

--- lesson4/classwork_3.py
+++ lesson4/classwork_3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function  # для совместимости с python 2.x.x
+
 # Дата некоторого дня определяется тремя натуральными числами y (год), m (месяц) и d (день).
 # По заданным d, m и y определите дату предыдщуего дня.
 #
@@ -10,28 +12,35 @@
 
 from classwork_2 import is_leap_year
 
+# lenOfMonths -> months_lengths
+months_lengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-lenOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 class NonValidDataExeption(Exception):
     pass
 
+
 def yesterday(year, month, day):
     try:
         if not ((0 <= int(year) <= 9999) and (0 < int(month) < 13) and (0 < int(day) < 32)):
-           print "Data isn't correct"
-           return None
+            print("Data isn't correct")  # мы договорились что пишем на 3м пайтоне => print()
+            # здесь по логике тоже надо скидывать NonValidDataExeption
+            return None
     except:
-        print "Data isn't integer number"
+        print("Data isn't integer number")
+        # здесь по логике тоже надо скидывать NonValidDataExeption
         return None
 
-    if day > lenOfMonths[month - 1]:
+    if day > months_lengths[month - 1]:
+        # в сообщении Exception нужно максимально детализировать ошибку
+        # - что было передано, какие значения допустимы
+        # NonValidDataExeption("Month {} isn't correct! Valid values is {}-{}".format(...))
         raise NonValidDataExeption("Month isn't correct")
 
     if is_leap_year(year):
-        lenOfMonths[1] = 29
+        months_lengths[1] = 29
     else:
-        lenOfMonths[1] = 28
+        months_lengths[1] = 28
 
     if (month == 1) and (day == 1):
         y = year - 1
@@ -41,10 +50,9 @@ def yesterday(year, month, day):
         y = year
         if day == 1:
             m = month - 1
-            d = lenOfMonths[m - 1]
+            d = months_lengths[m - 1]
         else:
             m = month
             d = day - 1
 
     return y, m, d
-
