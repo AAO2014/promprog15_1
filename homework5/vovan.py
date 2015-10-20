de15e377828621742f697f319a988de9da87ff85
wad <suguby@gmail.com>
2015-10-20 11:32:02 +0300

    надо избавится от глобальных переменных

--- homework5/vovan.py
+++ homework5/vovan.py
@@ -55,35 +55,44 @@
 
 from collections import  defaultdict
 
-ignore_sym = ['\n', ' ']
-frequency = defaultdict(int)
-print_matrix = []
+IGNORE_SYMBOLS = ['\n', ' ']  # это глобальная константа, по соглашению должна быть В ВЕРХНЕМ РЕГИСТРЕ
+print_matrix = []  # надо убрать, см ниже
 
 
 def read_file(filename):
+    # не нужно глобальных переменных, каждая функция возвращает результать своего труда,
+    # который идет параметром в другие функции
+    frequency = defaultdict(int)
     with open(filename, 'r') as input_file:
         for line in input_file:
             for char in line:
-                if char in ignore_sym:
+                if char in IGNORE_SYMBOLS:
                     continue
                 frequency[char] += 1
+    return frequency
 
 
-def calc_max_val():
+def calc_max_val(frequency):
     max_val = 0
     for k, v in frequency.items():
         if v > max_val:
             max_val = v
     return max_val
 
-def get_histogramm(max_val):
+
+def get_histogramm(frequency, max_val):
+    # эта должна формировать print_matrix
     sorted_chars = sorted(frequency)
     for key in sorted_chars:
         spaces = ' ' * (max_val - frequency[key])
         print_matrix.append(key + '#' * frequency[key] + spaces)
     return
 
-def print_histogramm(max_val):
+
+def print_histogramm(frequency, max_val):
+    # здесь frequency нужна только для вычисления своего размера,
+    # который зачем-то кстати вычисляется в цикле
+    # вынеси размер в параметр и вычисляй вовне
     for num_of_line_of_raster in range(max_val + 1):
         line_of_raster = ''
         for n_str in range(len(frequency)):
@@ -98,10 +107,7 @@ def print_histogramm(max_val):
 # это будет подготовкой к переходу к обьектному подходу в решении задачи
 
 if __name__ == '__main__':
-    read_file('text.txt')
-
-    max_val = calc_max_val()
-    
-    get_histogramm(max_val)
-
-    print_histogramm(max_val)
+    frequency = read_file('text.txt')
+    max_val = calc_max_val(frequency=frequency)
+    get_histogramm(frequency=frequency, max_val=max_val)
+    print_histogramm(frequency=frequency, max_val=max_val)
