65c00e895aeb5c0e3ba51528a46db8aec1326248
wad <suguby@gmail.com>
2015-11-02 19:00:22 +0300

    рекомендации к рефактору имен и организации данных

--- homework5/tests/histogram_tests.py
+++ homework5/tests/histogram_tests.py
@@ -8,11 +8,15 @@ from homework5.char_frequency_histogram_maker import CharFrequencyHistogramMaker
 # тесты лучше класть в отдельную папку-пакет tests
 # а вот файлы для них - еще в одну поддиректорию data к примеру
 # что бы тесты не перемешивались с данными для них
+# TODO удаляй мои коменты после реализации требований
 
 
 class CharFrequencyTest(unittest.TestCase):
 
     def assert_equal(self, textfile_1, testfile):
+        # название плохое - перекликается с self.assertEqual - я даже и не понял сразу %)
+        # переименуй - что делает эта функция? запускает код и сверяет с шаблоном.
+        # и параметры тоже переименовать бы - источник и результат
         v = CharFrequencyHistogramMaker()
         res = v.run(file_name=textfile_1)
         with open(testfile, 'r') as input_file:
@@ -27,17 +31,18 @@ class CharFrequencyTest(unittest.TestCase):
         # так что еще 5 тестов как минимум :)
         self.assert_equal('data/text_abc.txt', 'data/test_abc.txt')
 
-    # Первоначально пустой файл тест не проходил, так как функция вывода гистограмм всегда добавляла первод строки
-    # (сделал проверку и у последнего растра не добавляю перенос строк )
-    # !!!  но по условию задачи пустых файлов не бывает, минимум один непробельный (из разрешенных!) символ
+    # хорошо, тест на пустой файл опустим, пусть будет файл с одним пробелом
 
     def test_null(self):
         self.assert_equal('data/text_null.txt', 'data/test_null.txt')
 
-    # просится цикл для всех файлов начинающихся с text и test, но долго реализовывать
+    # циклы для тестов плохи в том плане, что из 25 вариантов входных файлов при рефакторе, предпложим,
+    # у тебя один сломается один - и как ты его будешь искать? лучше не ленится и писать отдельные функции :)
 
     def test_along_char(self):
         self.assert_equal('data/text_along_char.txt', 'data/test_along_char.txt')
+        # файлы называются тоже очень похоже te_xt и te_st - ты заставил меня подумать и поискать чем же они отличаются
+        # лучше пусть они начинаются с названия теста, а завершаются _src и _result так будет легче оринтироваться
 
     def test_only_empty_str(self):
         self.assert_equal('data/text_only_empty_str.txt', 'data/test_only_empty_str.txt')
