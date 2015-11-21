e30831f7551a7af11bb59069f3b662015452237e
Avis <aao.t@yandex.ru>
2015-11-21 22:28:05 +0300

    baby-written histogram django application

--- test1/histogram_app/views.py
+++ test1/histogram_app/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
 # Create your views here.
 from django.views.generic import TemplateView
 from histogram_app.char_frequency_histogram_maker import CharFrequencyHistogramMaker
@@ -16,18 +15,19 @@ class HistogramView(TemplateView):
         context['histogram_answer'] = 'Text for test'
         return context
 
+
 def get_histogram(request):
+    test_val = ''
     if request.method == 'POST':
         form = HistogramMainForm(request.POST)
         if form.is_valid():
             if form.fields['text_for_make_histogram'] != '':
-                # v = CharFrequencyHistogramMaker()
-                # form.fields['histogram_output'].initial = v.run(form.fields['text_for_make_histogram'])
-                form.fields['histogram_output'] = form.fields['text_for_make_histogram']
+                v = CharFrequencyHistogramMaker()
+                request.session['var'] = v.run(form.cleaned_data['text_for_make_histogram'])
             return HttpResponseRedirect('/histogram/')
-
     else:
+        if 'var' in request.session:
+            test_val = request.session['var']
+            del request.session['var']
         form = HistogramMainForm()
-
-    return render(request, 'histogram.html', {'form': form})
-
+    return render(request, 'histogram.html', {'form': form, 'histogram_output': test_val})
