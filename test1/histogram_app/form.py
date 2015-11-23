4f986216a063b6ed59225a574608df3638403a83
Avis <aao.t@yandex.ru>
2015-11-23 22:12:31 +0300

    Ввел CSS, улучшил интерфейс, без правок замечаний Вадима

--- test1/histogram_app/form.py
+++ test1/histogram_app/form.py
@@ -2,6 +2,7 @@ from django import forms
 
 
 class HistogramMainForm(forms.Form):
-    text_for_make_histogram = forms.CharField(label='Введите текст для построения частотной гистограммы', max_length=400)
+    text_for_make_histogram = forms.CharField(label='1) Введите текст и нажмите кнопку:',
+                                              max_length=400, widget=forms.Textarea(attrs={'cols':"40", 'rows':"10"}))
     # histogram_output = forms.CharField(label='Частотная диаграмма')
 
